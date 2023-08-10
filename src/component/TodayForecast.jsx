939790e161b6f3da6c752d016a92b7af9f7c6ede
Ravindu Adassuriya <ravinduadassuriya59@gmail.com>
2023-08-11 00:09:55 +0530

    todayForecast updated

--- src/component/TodayForecast.jsx
+++ src/component/TodayForecast.jsx
@@ -9,14 +9,20 @@ const TodayForecast = () => {
             <img src="https://o.remove.bg/downloads/39ae67c4-b42f-4bab-9e40-b4b63153e1df/sun-removebg-preview.png" alt="" className='w-20' />
             <h4 className='text-6xl text-white font-normal'>20°</h4>
             <div className='flex flex-col items-start'>
-               <p className='text-white mb-1'>Real Feel: <span className='font-semibold'>19°</span></p>
-               <p className='text-white mb-1'>Humidity: <span className='font-semibold'>42%</span></p>
-               <p className='text-white mb-1'>Wind: <span className='font-semibold'>2km/h</span></p>
+               <p className='text-white mb-1'>Real Feel: <span className='font-semibold ms-2' >19°</span></p>
+               <p className='text-white mb-1'>Humidity: <span className='font-semibold ms-2'>42%</span></p>
+               <p className='text-white mb-1'>Wind: <span className='font-semibold ms-2'>2km/h</span></p>
             </div>
          </div>
 
-         <div>
-
+         <div className='flex flex-row justify-center mt-6'>
+            <p className='text-white mb-1'>Rise: <span className='font-medium ms-2'>04:50 AM</span></p>
+            <p className='mx-3 text-white'>|</p>
+            <p className='text-white mb-1'>Set: <span className='font-medium ms-2'>06:17 PM</span></p>
+            <p className='mx-3 text-white'>|</p>
+            <p className='text-white mb-1'>High: <span className='font-medium ms-2'>22°</span></p>
+            <p className='mx-3 text-white'>|</p>
+            <p className='text-white mb-1'>Low: <span className='font-medium ms-2'>18°</span></p>
          </div>
       </div>
    )
