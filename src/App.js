7d28c96819fde7684557917c0797747153258999
Ravindu Adassuriya <ravinduadassuriya59@gmail.com>
2023-08-10 23:38:12 +0530

    date and location done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import DateAndLocation from './component/DateAndLocation';
 import Navbar from './component/Navbar';
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className='mx-auto max-w-screen-md mt-6 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 shadow-xl h-fit shadow-gray-400'>
       <Navbar />
+      <DateAndLocation/>
     </div>
   );
 }
---
+++ src/component/DateAndLocation.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const DateAndLocation = () => {
+  return (
+    <div className='mt-8 mb-8 text-center text-white'>
+        <div className='flex flex-row justify-center items-center'>
+           <h4 className='font-extralight text-xl'>Tuesday, 31 May 2022</h4>
+           <p className='mx-5'>|</p>
+           <h4 className='font-extralight text-xl'>Local Time: 03:04PM</h4>
+        </div>
+        <h4 className='mt-6 text-2xl font-semibold'>Berlin, GE</h4>
+    </div>
+  )
+}
+
+export default DateAndLocation
