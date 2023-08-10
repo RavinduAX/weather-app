b87b06919f93d1d1395e21e324ef9e044bc3d254
Ravindu Adassuriya <ravinduadassuriya59@gmail.com>
2023-08-11 00:17:26 +0530

    forecast content udpated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import DateAndLocation from './component/DateAndLocation';
+import Forecast from './component/Forecast';
 import Navbar from './component/Navbar';
 import TodayForecast from './component/TodayForecast';
 
@@ -9,7 +10,8 @@ function App() {
     <div className='mx-auto max-w-screen-md mt-6 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 shadow-xl h-fit shadow-gray-400'>
       <Navbar />
       <DateAndLocation />
-      <TodayForecast/>
+      <TodayForecast />
+      <Forecast title={'hourly forecast'} />
     </div>
   );
 }
---
+++ src/component/Forecast.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Forecast = ({title}) => {
+  return (
+    <div className='mt-5'>
+        <h4 className='text-white uppercase font-medium text-start border-b pb-1'>{title}</h4>
+        
+    </div>
+  )
+}
+
+export default Forecast
