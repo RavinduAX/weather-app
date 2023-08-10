567d71e59cb2624081c2b28045942f3b165ad41a
Ravindu Adassuriya <ravinduadassuriya59@gmail.com>
2023-08-10 23:17:48 +0530

    navbar done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navbar from './component/Navbar';
 function App() {
   return (
     <div className='mx-auto max-w-screen-md mt-6 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 shadow-xl h-fit shadow-gray-400'>
-      <Navbar/>
+      <Navbar />
     </div>
   );
 }
