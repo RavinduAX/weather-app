f7873e773c8e178682902abc03936105ba152fb9
Ravindu Adassuriya <ravinduadassuriya59@gmail.com>
2023-08-13 11:12:30 +0530

    content updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import DateAndLocation from './component/DateAndLocation';
 import Forecast from './component/Forecast';
 import Navbar from './component/Navbar';
 import TodayForecast from './component/TodayForecast';
+import axios from 'axios';
 
 
 function App() {
 
   const [location, setLocation] = useState('')
-  
+  const [data, setData] = useState({})
+
   const getData = (data) => {
     setLocation(data);
   }
 
+  useEffect(() => {
+    axios.get(`http://api.weatherapi.com/v1/forecast.json?key=a393728118084352b3571035231005&q=${location}`)
+      .then(res => setData(res.data))
+      .catch(err => console.log(err))
+  }, [location])
+
   return (
     <div className='mx-auto max-w-screen-md mt-6 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 shadow-xl h-fit shadow-gray-400'>
       <Navbar getData={getData} />
-      <DateAndLocation location={location} />
+      <DateAndLocation data={data} />
       <TodayForecast />
       <Forecast title={'hourly forecast'} />
     </div>
--- src/component/DateAndLocation.jsx
+++ src/component/DateAndLocation.jsx
@@ -1,24 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React from 'react'
 
-const DateAndLocation = ({ location }) => {
+const DateAndLocation = ({ data }) => {
 
-  const city = location;
-  const [data, setData] = useState({ name: '', country: '' })
-
-  useEffect(() => {
-     axios.get(`http://api.weatherapi.com/v1/forecast.json?key=a393728118084352b3571035231005&q=${location}`)
-      .then(res => { setData({ name: res.data.location.name, country: res.data.location.country }); console.log('dd') })
-      .catch(err => console.log(err))
-  }, [city])
+  
 
   return (
     <div className='mt-8 mb-7 text-center text-white'>
       <div className='flex flex-row justify-center items-center'>
-        <h4 className='font-extralight text-xl'>{ }</h4>
+        <h4 className='font-extralight text-xl'>{}</h4>
       </div>
 
-      <h4 className='mt-4 text-2xl font-semibold'>{data.name} | {data.country}</h4>
+      <h4 className='mt-4 text-2xl font-semibold'>{data.location && data.location.name} | {data.location && data.location.country }</h4>
     </div>
   )
 }
