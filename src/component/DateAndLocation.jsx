d0296b0057e5537168c3a186bb636cd46968943d
Ravindu Adassuriya <ravinduadassuriya59@gmail.com>
2023-08-13 11:14:17 +0530

    date builder

--- src/component/DateAndLocation.jsx
+++ src/component/DateAndLocation.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 
 const DateAndLocation = ({ data }) => {
 
-  
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }  
 
   return (
     <div className='mt-8 mb-7 text-center text-white'>
       <div className='flex flex-row justify-center items-center'>
-        <h4 className='font-extralight text-xl'>{}</h4>
+        <h4 className='font-extralight text-xl'>{dateBuilder(new Date())}</h4>
       </div>
 
       <h4 className='mt-4 text-2xl font-semibold'>{data.location && data.location.name} | {data.location && data.location.country }</h4>
