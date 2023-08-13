ea7333adc981da555fc21862ed3a4cd8a9c1bc19
Ravindu Adassuriya <ravinduadassuriya59@gmail.com>
2023-08-13 11:47:23 +0530

    today forecast done

--- src/component/TodayForecast.jsx
+++ src/component/TodayForecast.jsx
@@ -1,35 +1,39 @@
 import React from 'react'
 
 const TodayForecast = ({ data }) => {
-   
+
    //data.current
-   let fData = '' 
+   let cData = '' 
+   let fData = ''
    if (data !== 'undefined') {
-      fData = data.current;
+      cData = data.current;
+      fData = data.forecast;
    }
 
    return (
       <div className='text-center'>
-         <h4 className='text-cyan-300 text-xl mb-4'>{fData && fData.condition.text}</h4>
+         <h4 className='text-cyan-300 text-xl mb-4'>{cData && cData.condition.text}</h4>
 
          <div className='flex flex-row justify-between items-center'>
-            <img src={fData && fData.condition.icon} alt="" className='w-20' />
-            <h4 className='text-6xl text-white font-normal'>20°</h4>
+            <img src={cData && cData.condition.icon} alt="" className='w-20' />
+            <h4 className='text-6xl text-white font-normal'>{cData && cData.temp_c}°C</h4>
             <div className='flex flex-col items-start'>
-               <p className='text-white mb-1'>Real Feel: <span className='font-semibold ms-2' >19°</span></p>
-               <p className='text-white mb-1'>Humidity: <span className='font-semibold ms-2'>42%</span></p>
-               <p className='text-white mb-1'>Wind: <span className='font-semibold ms-2'>2km/h</span></p>
+               <p className='text-white mb-1'>Real Feel: <span className='font-semibold ms-2' >{cData && cData.feelslike_c}°C</span></p>
+               <p className='text-white mb-1'>Humidity: <span className='font-semibold ms-2'>{cData && cData.humidity}%</span></p>
+               <p className='text-white mb-1'>Wind: <span className='font-semibold ms-2'>{cData && cData.wind_kph}km/h</span></p>
             </div>
          </div>
 
          <div className='flex flex-row justify-center mt-6'>
-            <p className='text-white mb-1'>Rise: <span className='font-medium ms-2'>04:50 AM</span></p>
+            <p className='text-white mb-1'>Rise: <span className='font-medium ms-2'>{fData && fData.forecastday[0].astro.sunrise}</span></p>
             <p className='mx-3 text-white'>|</p>
-            <p className='text-white mb-1'>Set: <span className='font-medium ms-2'>06:17 PM</span></p>
+            <p className='text-white mb-1'>Set: <span className='font-medium ms-2'>{fData && fData.forecastday[0].astro.sunset}</span></p>
             <p className='mx-3 text-white'>|</p>
-            <p className='text-white mb-1'>High: <span className='font-medium ms-2'>22°</span></p>
+            <p className='text-white mb-1'>Rain: <span className='font-medium ms-2'>{fData && fData.forecastday[0].day.daily_chance_of_rain}%</span></p>
             <p className='mx-3 text-white'>|</p>
-            <p className='text-white mb-1'>Low: <span className='font-medium ms-2'>18°</span></p>
+            <p className='text-white mb-1'>Max: <span className='font-medium ms-2'>{fData && fData.forecastday[0].day.maxtemp_c}°C</span></p>
+            <p className='mx-3 text-white'>|</p>
+            <p className='text-white mb-1'>Min: <span className='font-medium ms-2'>{fData && fData.forecastday[0].day.mintemp_c}°C</span></p>
          </div>
       </div>
    )
